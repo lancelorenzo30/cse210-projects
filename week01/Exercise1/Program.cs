9b334b60d9f185176951e7eaad34e290492be57e
RozenkranLorenzo <rozenkranlorenzo1730@gmail.com>
2026-01-08 21:22:27 +0800

    Corrected variable name convention.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,11 +7,11 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
         Console.Write("What is your first name? ");
-        string fname = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string lname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
