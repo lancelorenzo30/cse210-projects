64b00dcb5efa13f4c253c5f1ee69781a9093edc7
RozenkranLorenzo <rozenkranlorenzo1730@gmail.com>
2026-01-10 22:07:01 +0800

    Learning activity 3, Loops.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(0,100);
+
+        int guessNumber = 0;
+
+        do
+        {            
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed right!");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }  while (guessNumber != magicNumber);
     }
 }
