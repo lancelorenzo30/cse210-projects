55fe01a0c4d7fba22bbc6263c4dce638e9c99e2e
RozenkranLorenzo <rozenkranlorenzo1730@gmail.com>
2026-01-15 21:25:35 +0800

    Finished Week 2 Activity Learning Class and abstraction.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Lance Lorenzo";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+       resume1.displayResume();
     }
 }
