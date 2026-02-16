18a6517ed2733f899c5533c2c15d6e26aaca2d92
RozenkranLorenzo <rozenkranlorenzo1730@gmail.com>
2026-02-16 20:38:59 +0800

    Polymorphism Mini Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+
+        Square s1 = new Square("blue", 5);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("green", 3, 4);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("bluegreen", 6);
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
